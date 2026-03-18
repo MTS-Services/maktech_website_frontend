3ed8b461393c8d57d19e2ba5dac96ef477126ff9
shanjid5566 <shanjidahmed66@gmail.com>
2026-03-18 10:50:07 +0600

    fix: increase padding in AboutSection component for improved layout

--- src/pages/home/components/AboutSection.jsx
+++ src/pages/home/components/AboutSection.jsx
@@ -9,7 +9,7 @@ const AboutSection = () => {
   ];
 
   return (
-    <section className='hidden xl:block w-full bg-white text-black py-20 relative z-10'>
+    <section className='hidden xl:block w-full bg-white text-black py-24 relative z-10'>
       <div className='container mx-auto px-5 xl:px-8 2xl:px-12'>
         {/* Top Content (Image + Text) */}
         <div className='flex items-center gap-12 xl:gap-20 mb-16'>
